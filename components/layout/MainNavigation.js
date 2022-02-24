bb1f5c4c8eaf2ad36a3f1dd54bf4683efe2bb050
Rtiik Gupta <ritikgupta9414@gmail.com>
2022-02-24 13:31:17 +0530

    Signin auth added with nextAuth.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,7 +1,9 @@
-import Link from 'next/link'
-import classes from './MainNavigation.module.css';
+import Link from "next/link";
+import classes from "./MainNavigation.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function MainNavigation() {
+  const { data: session } = useSession();
 
   return (
     <header className={classes.header}>
@@ -9,11 +11,42 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/'>All Meetups</Link>
+            <Link href="/">All Meetups</Link>
           </li>
-          <li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
-          </li>
+
+          {!session && (
+            <li style={{marginRight:'3rem'}}>
+              <Link href="/api/auth/signin">
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signIn();
+                  }}
+                >
+                  Signin
+                </a>
+              </Link>
+            </li>
+          )}
+          {session && (
+            <div className={classes.signin}>
+              <li>
+                <Link href="/new-meetup">Add New Meetup</Link>
+              </li>
+              <li>
+                <Link href="/api/auth/signout">
+                  <a
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signOut();
+                    }}
+                  >
+                    Sign Out
+                  </a>
+                </Link>
+              </li>
+            </div>
+          )}
         </ul>
       </nav>
     </header>
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import Layout from '../components/layout/Layout';
+import "../styles/globals.css";
+import Layout from "../components/layout/Layout";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Layout> <Component {...pageProps} /></Layout>
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
